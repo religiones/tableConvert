925e9c0d6178354da2882d9c1c920d4fbb35c910
Kronos <1963024305@qq.com>
2022-03-18 15:55:28 +0800

    commit

---
+++ src/components/Table.tsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Table extends Component<any, any> {
+    constructor(props: any){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div style={{width:'100%', height:'100%'}}></div>
+        )
+    }
+}
+
+export default Table
